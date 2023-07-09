d14a174b3d0a11137b44f11dc170c45cf5c4b294
isksss <104404522+isksss@users.noreply.github.com>
2023-07-10 02:22:24 +0900

    get restart time

--- commands/start.go
+++ commands/start.go
@@ -3,8 +3,11 @@ package commands
 import (
 	"context"
 	"flag"
+	"fmt"
+	"time"
 
 	"github.com/google/subcommands"
+	"github.com/isksss/paperma-manager/config"
 )
 
 type StartCommand struct {
@@ -24,5 +27,24 @@ func (c *StartCommand) Execute(ctx context.Context, f *flag.FlagSet, args ...int
 	// - javaの存在確認
 	// - サブプロセスでPaperMC実行
 	// - configで指定された時間に再起動
+
+	// Get restart time.
+	data, err := config.GetConfig()
+	if err != nil {
+		fmt.Printf("config read error: %v\n", err)
+		return subcommands.ExitFailure
+	}
+
+	var parsedTime []time.Time
+	for _, restartTime := range data.Server.RestartTime {
+		parsed, err := time.Parse("15:04", restartTime)
+		if err != nil {
+			continue
+		}
+
+		parsedTime = append(parsedTime, parsed)
+	}
+
+	fmt.Println(parsedTime)
 	return subcommands.ExitSuccess
 }
